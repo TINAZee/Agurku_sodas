bcae9e05cc1d21c4394262ea63e8ec610e7da6d4
Kristina Piekutė <70894872+TINAZee@users.noreply.github.com>
2020-12-28 17:31:54 +0200

    sodas

---
+++ skynimas.php
@@ -0,0 +1,74 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['a'])) {
+    $_SESSION['a'] = [];
+    $_SESSION['agurku ID'] = 0;
+}
+
+//SKINTI SCENARIJUS
+if (isset($_POST['skinti'])) {
+    foreach($_SESSION['a'] as $index => &$agurkas) {
+            $skynimas = $_POST['kiek_skinti'];
+                $agurkas['agurkai'] -= $_POST['kiek_skinti'][$agurkas['id']];
+    }
+    header('Location: http://localhost/BIT_KURSAI_PHP/Agurku_sodas/skynimas.php.');
+    exit;
+}
+
+
+//SKINTI VISUS SCENARIJUS
+
+if (isset($_POST['skinti_visus'])) {
+    foreach ($_SESSION['a'] as $index => &$agurkas ) {
+        if ($_POST['skinti_visus'] == $agurkas['id']) {
+            $agurkas['agurkai'] -=  $agurkas['agurkai'];
+        }
+    }
+    header('Location: http://localhost/BIT_KURSAI_PHP/Agurku_sodas/skynimas.php.');
+    die;
+}
+
+//NUIMTI VISA DERLIU SCENARIJUS
+
+if (isset($_POST['nuimtiDerliu'])) {
+    foreach($_SESSION['a'] as $index => &$agurkas) {
+        $agurkas['agurkai'] = 0;
+    }
+    header('Location: http://localhost/BIT_KURSAI_PHP/Agurku_sodas/skynimas.php.');
+    exit;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skynimas</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+<header>
+<a href="sodinimas.php">Sodinimas</a>
+<a href="auginimas.php">Auginimas</a>
+<a href="skynimas.php">Skynimas</a>
+</header>
+<h1>Agurkų sodas</h1>
+<h3>Agurkų skynimas</h3>
+    <form action="" method="post">
+    <?php foreach($_SESSION['a'] as $agurkas): ?>
+    <div id = main>
+    <td><img class="img" src="<?= $agurkas['img'] ?>" alt="agurkas">
+    Agurkas Nr. <?= $agurkas['id'] ?>
+    <p style="display:inline;color:red"> Galima skinti </p>
+    <h1 style="display:inline;"><?= $agurkas['agurkai'] ?></h1>
+    <input type="text" name="kiek_skinti[<?= $agurkas['id'] ?>]"value=<?= $skynimas ?? 0 ?>>
+    <button type="submit" name="skinti">Skinti</button>
+    <button type="submit" name="skinti_visus" value="<?= $agurkas['id'] ?>">Skinti visus</button>
+    </div>
+    <?php endforeach ?>
+    <button type="submit" name="nuimtiDerliu">Nuimti visą derlių</button>
+    </form>
+</body>
+</html>
---
+++ sodinimas.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['a'])) {
+    $_SESSION['a'] = [];
+    $_SESSION['agurku ID'] = 0;
+}
+
+// SODINIMO SCENARIJUS
+if (isset($_POST['sodinti'])) {
+    $_SESSION['a'][] = [
+        'id' => ++$_SESSION['agurku ID'],
+        $img = ['./img/img_1.jpg', './img/img_2.jpg', './img/img_3.jpg','./img/img_4.jpg','./img/img_5.jpg'],
+        'img' => $img[array_rand($img)],
+        'agurkai' => 0
+    ];
+    header('Location:http://localhost/BIT_KURSAI_PHP/Agurku_sodas/sodinimas.php');
+    die;
+}
+// ISROVIMO SCENARIJUS
+if (isset($_POST['rauti'])) {
+    foreach($_SESSION['a'] as $index => $agurkas) {
+        if ($_POST['rauti'] == $agurkas['id']) {
+            unset($_SESSION['a'][$index]);
+            header('Location: http://localhost/BIT_KURSAI_PHP/Agurku_sodas/sodinimas.php');
+            die;
+        }
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sodinimas</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+<header>
+<a href="sodinimas.php">Sodinimas</a>
+<a href="auginimas.php">Auginimas</a>
+<a href="skynimas.php">Skynimas</a>
+</header>
+<h1>Agurkų sodas</h1>
+<h3>Sodinimas</h3>
+    <form action="" method="post">
+    <?php foreach($_SESSION['a'] as $agurkas): ?>
+    <div id = main>
+    <img class="img" src="<?= $agurkas['img'] ?>" alt="agurkas">
+    <p>Agurkas nr. <?= $agurkas['id'] ?></p>
+    Agurkų: <?= $agurkas['agurkai'] ?>
+    <button type="submit" name="rauti" value="<?= $agurkas['id'] ?>">Išrauti</button>
+    </div>
+
+    <?php endforeach ?>
+    <button type="submit" name="sodinti">SODINTI</button>
+    </form>
+</body>
+</html>
